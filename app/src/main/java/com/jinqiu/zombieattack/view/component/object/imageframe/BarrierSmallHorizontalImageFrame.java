98fae305067832fbd3c28e38949064c6505c0b01
Jinqiu Deng <deng.jinqiu@gmail.com>
2016-05-21 14:47:37 -0400

    Change to android studio project

---
+++ app/src/main/java/com/jinqiu/zombieattack/view/component/object/imageframe/BarrierSmallHorizontalImageFrame.java
@@ -0,0 +1,12 @@
+package com.jinqiu.zombieattack.view.component.object.imageframe;
+
+import com.jinqiu.zombieattack.R;
+
+public class BarrierSmallHorizontalImageFrame extends ImageFrame {
+
+	public BarrierSmallHorizontalImageFrame() {
+		imageList.add(R.drawable.barrier_small_horizontal_1);
+		imageList.add(R.drawable.barrier_small_horizontal_2);
+	}
+
+}
---
+++ app/src/main/java/com/jinqiu/zombieattack/view/component/object/imageframe/ScreenBoundImageFrame.java
@@ -0,0 +1,11 @@
+package com.jinqiu.zombieattack.view.component.object.imageframe;
+
+import com.jinqiu.zombieattack.R;
+
+public class ScreenBoundImageFrame extends ImageFrame {
+
+	public ScreenBoundImageFrame() {
+		imageList.add(R.drawable.blank);
+	}
+
+}
